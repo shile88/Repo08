7fa1f8087fa2b46ccf296edf25c685e43d23b010
shile88 <milos.ivanis@gmail.com>
2022-04-04 11:38:17 +0200

    Function with 2 parameters

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+function functionPrintInfo(parameter1, parameter2){
+    console.log(parameter1, parameter2);
+}
+
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+
+module.exports = functionPrintInfo;
